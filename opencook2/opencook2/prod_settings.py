fb29498f9c2f46ef9c58e240068b46bb45a46ea7
1105104230 <45057687+1105104230@users.noreply.github.com>
2019-05-09 16:44:30 +0800

    626

---
+++ opencook2/opencook2/prod_settings.py
@@ -0,0 +1,16 @@
+from .settings import *
+import dj_database_url
+
+#把debug模式關掉
+DEBUG = False
+#遵守 HTTPS 連線中的 "X-Forwarded-Proto" header
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
+#設定靜態檔案位置
+STATIC_ROOT = 'staticfiles'
+#設定資料庫
+DATABASE ={
+	'default' : dj_database_url.config()
+}
+#允許所有網址連至網站
+ALLOWED_HOSTS = ['*']
+
